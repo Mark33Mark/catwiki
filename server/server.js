7be6e0d0fe9f179324854d0746409939931baf47
Mark33Mark <mark@watsonised.com>
2022-02-23 14:00:08 +1100

    More direct pointing to wildcard route in the server.js file

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === "production") {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // required for Lighthouse PWA test app to be installable.
